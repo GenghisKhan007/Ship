bfce29210fc92d47c8bf66f9efd0fc2cdab10974
GenghisKhan007 <88216463+GenghisKhan007@users.noreply.github.com>
2021-09-10 17:49:11 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,28 @@
+var ship,shipImage
+var sea,seaImage
+
+function preload(){
+seaImage = loadImage("sea.png")
+shipImage = loadAnimation("ship-1.png","ship-2.png","ship-3.png","ship-4.png")
+
+}
+
+function setup(){
+  createCanvas(400,400);
+  sea = createSprite(400,200);
+  sea.addImage(seaImage);
+  sea.scale = 0.3
+  sea.velocityX = -3
+
+  ship = createSprite(130,200);
+  ship.addAnimation("ship",shipImage)
+  ship.scale = 0.3
+}
+
+function draw() {
+  background("blue");
+  if(sea.x<0){
+    sea.x = sea.width/8
+  }
+ drawSprites();
+}
